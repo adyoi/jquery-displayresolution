32a6ae3e159b5539775e11079f5cda002409d0be
Adi Apriyanto <adyoi@users.noreply.github.com>
2019-07-08 02:31:27 +0700

    UPDATE
    UPDATE

---
+++ jquery.displayresolution.js
@@ -0,0 +1,65 @@
+(function(window, $){
+
+// Jquery Display Resolution
+// Created and Design by Adi Apriyanto
+// Tangerang Selatan, 8 July 2019 | 2:25 AM
+
+$.fn.displayresolution = function(options){
+var t = this;
+var w = $(window);
+var s = $.extend({
+background: "black",
+color: "white",
+opacity: 0.5,
+width: 300
+}, options);
+var o = $.extend({
+font: "Helvetica",
+size: "12px",
+sep: " | ",
+}, options);
+t.css({
+top: 0,
+right: 0,
+left: 0,
+padding: 6,
+zIndex: 999999,
+marginRight: "auto",
+marginLeft: "auto",
+textAlign: "center",
+position: "fixed",
+opacity: s.opacity,
+width: s.width,
+'font-family': o.font,
+'font-size': o.size,
+background: s.background,
+color: s.color,
+cursor: "pointer"
+});
+t.each(function(){
+$(t).text('Scroll Top: ' + w.scrollTop() + o.sep +
+'Width: ' + w.width() + ' px' + o.sep +
+'Height: ' + w.height() +' px');
+});
+w.scroll(function(){
+$(t).text('Scroll Top: ' + w.scrollTop() + o.sep +
+'Width: ' + w.width() + ' px' + o.sep +
+'Height: ' + w.height() +' px');
+});
+w.resize(function(){
+$(t).text('Scroll Top: ' + w.scrollTop() + o.sep +
+'Width: ' + w.width() + ' px' + o.sep +
+'Height: ' + w.height() +' px');
+});
+$(t).click(function(e){
+var $temp = $('<input>');
+$('body').append($temp);
+$temp.val('top: ' + w.scrollTop() + "; " +
+'width: ' + w.width() + 'px' + "; " +
+'height: ' + w.height() +'px' + "; ").select();
+document.execCommand('copy');
+$temp.remove();
+$(t).text('copied');
+});
+};
+}(window, jQuery));
